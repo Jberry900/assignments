76b1d9def4df812367f344b66a51d9edcd898a9a
Josh Berry <j.berry091390@gmail.com>
2022-07-27 20:33:24 -0500

    pushing everything up until the rpg

---
+++ exercises/bugs-on-a-plane/app.js
@@ -0,0 +1,14 @@
+var form = document.getElementById("airline-form");
+var submit = document.getElementById("submit");
+//var query = document.querySelector;
+function formAlert() {
+    var firstName = form.elements["first-name"].value;
+    var lastName = form.elements["last-name"].value;
+    var age = form.elements["age"].value;
+    var gender = form.elements["gender"].value;
+    var location = form.elements["travel-location"].value;
+    var diet = form.elements["vegan"].value;
+    
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
+document.getElementById("submit").addEventListener("click", formAlert);
